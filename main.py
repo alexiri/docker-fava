6efd61012720456f77732adafb0d759adfc4637a
Alex Iribarren <Alex.Iribarren@gmail.com>
2018-08-20 18:25:08 +0200

    Added prefix

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import os
 from fava.application import app
+from fava.util import simple_wsgi
+from werkzeug.wsgi import DispatcherMiddleware
 
 app.config['BEANCOUNT_FILES'] = os.environ['BEANCOUNT_INPUT_FILE'].split(',')
+prefix = os.environ['PREFIX']
+
+if prefix:
+    app.wsgi_app = DispatcherMiddleware(simple_wsgi, {prefix: app.wsgi_app})
