27b29f66128962257447552d66a6d19a8c6b354c
Alex Iribarren <alex.iribarren@gmail.com>
2019-11-09 13:19:51 +0100

    Don't fail if we didn't receive a prefix

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fava.util import simple_wsgi
 from werkzeug.wsgi import DispatcherMiddleware
 
 app.config['BEANCOUNT_FILES'] = os.environ['BEANCOUNT_INPUT_FILE'].split(',')
-prefix = os.environ['PREFIX']
+prefix = os.environ.get('PREFIX')
 
 if prefix:
     app.wsgi_app = DispatcherMiddleware(simple_wsgi, {prefix: app.wsgi_app})
